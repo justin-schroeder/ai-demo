59fe146a8682bafdb5763bade3fb9f42c3bdc36c
Justin Schroeder <justin@formkit.com>
2023-11-16 16:57:12 -0500

    chore: basic post structure

--- app.vue
+++ app.vue
@@ -1,7 +1,44 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  title: 'Create a new post',
+})
+
+const isLoading = ref(true)
+</script>
 
 <template>
   <PostEditor>
-    <FormKit type="textarea" name="post" />
+    <FormKit type="form" #default="{ state: { valid } }" :actions="false">
+      <FormKit
+        type="textarea"
+        name="post"
+        label="Blog post"
+        help="Enter you blog post here, markdown formatting is acceptable."
+        validation="required"
+      />
+      <FormKit
+        type="text"
+        name="title"
+        label="Title"
+        help="Enter a title for your blog post."
+        :prefix-icon="isLoading ? 'spinner' : null"
+        prefix-icon-class="animate-spin"
+        validation="required"
+      />
+      <FormKit
+        type="text"
+        name="slug"
+        label="Slug"
+        help="Enter a unique page slug for your blog post."
+        :prefix-icon="isLoading ? 'spinner' : null"
+        prefix-icon-class="animate-spin"
+        validation="required"
+      />
+      <FormKit
+        type="submit"
+        label="Save post"
+        :disabled="isLoading || !valid"
+      />
+    </FormKit>
   </PostEditor>
 </template>
