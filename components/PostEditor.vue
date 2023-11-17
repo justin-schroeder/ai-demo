23c06085d150aa5ef6b536e239b4099096942733
Justin Schroeder <justin@formkit.com>
2023-11-16 22:39:11 -0500

    chore: adds logo to page

--- components/PostEditor.vue
+++ components/PostEditor.vue
@@ -2,8 +2,9 @@
 
 <template>
   <div class="flex flex-col justify-center">
+    <TheLogo />
     <div
-      class="bg-white shadow-2xl rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 mx-auto w-1/2 mt-10 min-w-[30em]"
+      class="bg-white shadow-2xl rounded px-8 pt-6 pb-8 mb-4 flex flex-col mx-auto w-1/2 min-w-[30em]"
     >
       <h2 class="text-2xl mb-8">Create a new post</h2>
       <ClientOnly>
